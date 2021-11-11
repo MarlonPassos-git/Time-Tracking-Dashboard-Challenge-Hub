5edaa3f6bfbc1308fcd90b6a4b6af29620916ce8
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-11-11 20:00:36 -0300

    adicionando a estrutura do projeto

---
+++ src/pages/home page/app.tsx
@@ -0,0 +1,10 @@
+import './style.scss'
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
