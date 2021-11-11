fa845ae45e9597d9911a5ceb0651cea2bbe755d2
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-11-11 20:59:37 -0300

    adicionando o HTML do componente 'cardUser'

--- src/pages/home page/app.tsx
+++ src/pages/home page/app.tsx
@@ -1,10 +1,10 @@
+import { CardUser } from '../../components/CardUser';
 import './style.scss'
 
 export function App() {
   return (
-    <div>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <main>
+        <CardUser />
+    </main>
   );
 }
